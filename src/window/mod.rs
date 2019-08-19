e506dc7a4a5b435df65e2879c2b624cd04923f5b
Elena Gantner <elena-laura.gantner@linesofcode.at>
2019-08-19 18:22:59 +0200

    opengl window create

--- src/window/mod.rs
+++ src/window/mod.rs
@@ -6,11 +6,17 @@ pub mod linux;
 
 pub trait Window {
     fn create_window(width: i32, height: i32, name: &str, title: &str) -> Self;
+    #[cfg(target_os = "windows")]
     fn update(&self) -> bool;
+    #[cfg(target_os = "linux")]
+    fn update(&mut self) -> bool;
 
     fn get_width(&self) -> u32;
     fn get_height(&self) -> u32;
     
     #[cfg(target_os = "windows")]
     fn get_handle(&self) -> winapi::shared::windef::HWND;
+
+    #[cfg(target_os = "linux")]
+    fn get_handle(&self) -> &glium::Display;
 }
