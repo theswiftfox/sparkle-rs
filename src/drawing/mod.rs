9b537328d6232ff070931c57b1431ba3a9b5a484
Elena Gantner <elena-laura.gantner@linesofcode.at>
2019-11-01 13:53:56 +0100

    remove opengl stuff - for now no aim for cross platform (out of scope); enable alpha blending

--- src/drawing/mod.rs
+++ src/drawing/mod.rs
@@ -1,21 +1,185 @@
-use crate::input::input_handler::InputHandler;
-use crate::input::Camera;
-
 pub mod generate;
 pub mod geometry;
 pub mod scenegraph;
 
-#[cfg(target_os = "windows")]
-crate mod d3d11;
-#[cfg(target_os = "linux")]
-crate mod opengl;
+pub(crate) mod d3d11;
 
-pub trait Renderer {
-    fn create(width: i32, height: i32, title: &str) -> Self;
-    fn cleanup(&mut self);
-    fn update(&mut self) -> Result<bool, Box<dyn std::error::Error>>;
-    fn change_camera(&mut self, cam: std::rc::Rc<std::cell::RefCell<dyn Camera>>);
-    fn change_input_handler(&mut self, handler: std::rc::Rc<std::cell::RefCell<dyn InputHandler>>);
+use crate::import;
+use crate::drawing::scenegraph::Scenegraph;
+use crate::input::first_person::FPSController;
+use crate::input::input_handler::InputHandler;
+use crate::input::Camera;
+use crate::window::Window;
+use std::time::Instant;
+use winapi::um::d3d11 as dx11;
+use d3d11::{shaders,cbuffer,D3D11Backend,DxError};
+
+struct ShaderUniforms {
+    pub view: glm::Mat4,
+    pub proj: glm::Mat4,
+}
+
+pub struct Renderer {
+    scene: Scenegraph,
+    draw_program: Option<shaders::ShaderProgram>,
+    input_handler: Option<std::rc::Rc<std::cell::RefCell<dyn InputHandler>>>,
+    camera: Option<std::rc::Rc<std::cell::RefCell<dyn Camera>>>,
+    shader_uniforms: cbuffer::CBuffer<ShaderUniforms>,
+    backend: D3D11Backend,
+    window: std::rc::Rc<std::cell::RefCell<Window>>,
+    clock: Instant,
+}
+
+impl Renderer
+{
+    pub fn create(width: i32, height: i32, title: &str) -> Renderer {
+        let input_handler =
+            FPSController::create_ptr((width as f32) / (height as f32), 45.0f32, 0.1f32, 100.0f32);
+
+        let window = Window::create_window(width, height, "main", title);
+        let backend = match D3D11Backend::init(window.clone()) {
+            Ok(b) => b,
+            Err(e) => panic!(format!("{}", e)),
+        };
+        let uniforms = ShaderUniforms {
+            view: glm::identity(),
+            proj: glm::identity(),
+        };
+        let cbuff =
+            match cbuffer::CBuffer::create(uniforms, backend.get_context(), backend.get_device()) {
+                Ok(b) => b,
+                Err(e) => panic!(e),
+            };
+        let mut renderer = Renderer {
+            backend: backend,
+            window: window,
+            draw_program: None,
+            scene: Scenegraph::empty(),
+            input_handler: None,
+            camera: None,
+            shader_uniforms: cbuff,
+            clock: Instant::now(),
+        };
+
+        let mut renderer = match renderer.init_draw_program() {
+            Ok(_) => renderer,
+            Err(e) => panic!(format!("{}", e)),
+        };
+
+        let node = import::load_gltf("assets/sponza_glTF/Sponza.gltf", renderer.backend.get_device(), renderer.backend.get_context()).expect("Unable to load scene");
+        renderer.scene.set_root(node);
+
+        renderer.change_input_handler(input_handler.clone());
+        renderer.change_camera(input_handler.clone());
+        renderer.shader_uniforms.data.proj = input_handler.borrow().projection_mat();
+
+        println!("DX Setup took {} ms", renderer.clock.elapsed().as_millis());
+        renderer.clock = Instant::now();
+        return renderer;
+    }
+
+    pub fn cleanup(&mut self) {
+        self.backend.cleanup();
+    }
+
+    pub fn update(&mut self) -> Result<bool, Box<dyn std::error::Error>> {
+        let dt = self.clock.elapsed().as_millis() as f32 / 1000f32;
+        self.clock = Instant::now();
+        let ok = self.window.borrow_mut().update();
+
+        if ok {
+            match &self.input_handler {
+                Some(i) => i.borrow_mut().update(dt),
+                None => {}
+            };
+            match &self.camera {
+                Some(c) => {
+                    c.borrow_mut().update(dt);
+                    self.shader_uniforms.data.view = c.borrow().view_mat();
+                }
+                None => {}
+            };
+            self.shader_uniforms.update()?;
+            self.render()?;
+        }
+        Ok(ok)
+    }
+
+    pub fn change_input_handler(&mut self, handler: std::rc::Rc<std::cell::RefCell<dyn InputHandler>>) {
+        match &self.input_handler {
+            Some(i) => {
+                std::mem::drop(i);
+                self.input_handler = Some(handler.clone());
+            }
+            None => self.input_handler = Some(handler.clone()),
+        };
+        self.window.borrow_mut().set_input_handler(handler.clone())
+    }
+    pub fn change_camera(&mut self, cam: std::rc::Rc<std::cell::RefCell<dyn Camera>>) {
+        match &self.camera {
+            Some(c) => {
+                std::mem::drop(c);
+                self.camera = Some(cam.clone());
+            }
+            None => self.camera = Some(cam.clone()),
+        }
+    }
+
+    /**
+     * Section: Render funcs
+     */
+    fn clear(&self) {
+        self.backend.pix_begin_event("Clear");
+
+        let ctx = self.backend.get_context();
+        let render_target = self.backend.get_render_target_view();
+        let depth_stencil = self.backend.get_depth_stencil_view();
+
+        let color: [f32; 4] = crate::drawing::colors_linear::background().into();
+        unsafe {
+            (*ctx).ClearRenderTargetView(render_target, &color);
+            (*ctx).ClearDepthStencilView(
+                depth_stencil,
+                dx11::D3D11_CLEAR_DEPTH | dx11::D3D11_CLEAR_STENCIL,
+                1.0f32,
+                0,
+            );
+
+            (*ctx).OMSetRenderTargets(1, &render_target, depth_stencil);
+        };
+        let viewport = self.backend.get_viewport();
+        unsafe { (*ctx).RSSetViewports(1, viewport) };
+
+        self.backend.pix_end_event();
+    }
+
+    fn render(&mut self) -> Result<(), DxError> {
+        self.clear();
+        match &self.draw_program {
+            Some(_) => self.draw_program.as_mut().unwrap().activate(),
+            None => {}
+        };
+        self.backend.pix_begin_event("Render");
+
+        let ctx = self.backend.get_context();
+        unsafe {
+            (*ctx).VSSetConstantBuffers(0, 1, &self.shader_uniforms.buffer_ptr() as *const *mut _)
+        };
+
+        self.scene.draw();
+
+        self.backend.pix_end_event();
+
+        self.backend.present()?;
+
+        Ok(())
+    }
+
+    fn init_draw_program(&mut self) -> Result<(), DxError> {
+        self.draw_program = Some(self.backend.create_shader_program()?);
+
+        Ok(())
+    }
 }
 
 #[allow(dead_code)] // we don't want warnings if some color is not used..
@@ -37,21 +201,10 @@ pub mod colors_linear {
     }
 }
 
-use crate::window::*;
-#[cfg(target_os = "linux")]
 pub fn create_backend(
     width: i32,
     height: i32,
     title: &str,
-) -> opengl::OpenGLRenderer<linux::WindowNix> {
-    opengl::OpenGLRenderer::create(width, height, title)
-}
-
-#[cfg(target_os = "windows")]
-pub fn create_backend(
-    width: i32,
-    height: i32,
-    title: &str,
-) -> d3d11::D3D11Renderer<windows::WindowWin> {
-    d3d11::D3D11Renderer::create(width, height, title)
+) -> Renderer {
+    Renderer::create(width, height, title)
 }
