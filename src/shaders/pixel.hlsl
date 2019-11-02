7fff312a6141b074e067a23a1011d16dc620eb6d
Patrick Gantner <patrick.gantner@linesofcode.at>
2019-11-02 02:30:32 +0100

    load normal and metallic-roughness texture

--- src/shaders/pixel.hlsl
+++ src/shaders/pixel.hlsl
@@ -11,9 +11,15 @@ struct PX_OUT {
 
 Texture2D txDiffuse : register(t0);
 SamplerState samplerLinear : register(s0);
+Texture2D txMetallicRoughness : register(t1);
+SamplerState samplerMR: register(s1);
+Texture2D txNormal : register(t2);
+SamplerState samplerNormal: register(s2);
 
 PX_OUT main(PX_IN input) {
 	PX_OUT output;
 	output.color = txDiffuse.Sample(samplerLinear, input.txCoord);
+	float2 mr_tex = txMetallicRoughness.Sample(samplerMR, input.txCoord).gb;
+	float3 normal = txNormal.Sample(samplerNormal, input.txCoord).xyz;
 	return output;
 }
